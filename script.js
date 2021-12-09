5031b7bebd5f02f22f62ede5d82812db6eba3d9f
ladan <ladan.ghanavati97@gmail.com>
2021-12-09 12:24:34 +0330

    finish

---
+++ script.js
@@ -0,0 +1,8 @@
+function show() {
+    let menuShow = document.getElementById("menu");
+    if (menuShow.style.display === "block") {
+        menuShow.style.display = "none";
+    } else {
+        menuShow.style.display = "block";
+    }
+  }
